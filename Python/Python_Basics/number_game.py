092d1189dab948199089f27387ff6c2ada4c1110
Ronald Williams <ronaldnwilliams@gmail.com>
2018-01-19 16:59:24 -0600

    Python: number_game - Added feature; keep track and tell player how many guesses it took them.

--- Python/Python_Basics/number_game.py
+++ Python/Python_Basics/number_game.py
@@ -4,14 +4,16 @@ import random
 secret_num = random.randint(1, 10)
 
 def main():
+    count = 0
     while True:
         # get a number guess from the player
         guess = int(input("Guess a number between 1 and 10: "))
-
+        # keep track of how many guesses
+        count += 1
         # compare guess to number
         if guess == secret_num:
             # print hit or miss
-            print("You got it! My number was {}".format(secret_num))
+            print("You got it! My number was {} and you guessed it in {} {try_plural}".format(secret_num, count, try_plural="try" if count == 1 else "tries"))
             break
         else:
             print("That's not it!")
