ee36ba8b1d51d24ee89c2430c9929ab4ff77f1af
Ronald Williams <ronaldnwilliams@gmail.com>
2018-01-19 18:31:37 -0600

    Python - number_game: Added features; limit number of guesses, catch non-integers, give a hint, and let play again.

--- Python/Python_Basics/number_game.py
+++ Python/Python_Basics/number_game.py
@@ -1,26 +1,40 @@
 import random
 
-# generate a random number between 1 and 10
-secret_num = random.randint(1, 10)
-
-def main():
-    count = 0
-    while True:
+def game():
+        
+    # generate a random number between 1 and 10
+    secret_num = random.randint(1, 10)
+    guesses = []
+    
+    while len(guesses) < 3:
+    
         # get a number guess from the player
-        guess = int(input("Guess a number between 1 and 10: "))
-        # keep track of how many guesses
-        count += 1
-        # compare guess to number
-        if guess == secret_num:
-            # print hit or miss
-            print("You got it! My number was {} and you guessed it in {} {try_plural}".format(secret_num, count, try_plural="try" if count == 1 else "tries"))
-            break
-        else:
-            print("That's not it!")
-            # give a hint
-            if guess > secret_num:
-                print("Hint: Guess lower.")
+        try:
+            guess = int(input("Guess a number between 1 and 10: "))
+        except ValueError:
+            print("That's not a number.")
+        else:        
+            # keep track of how many guesses
+            # compare guess to number
+            if guess == secret_num:
+                # print hit or miss
+                print("You got it! My number was {} and you guessed it in {} {try_plural}".format(secret_num, len(guesses) + 1 , try_plural="try" if len(guesses) == 0 else "tries"))
+                break                    
             else:
-                print("Hint: Guess higher.")
+                print("That's not it!")
+                guesses.append(guess)
+                # give a hint
+                if len(guesses) != 3:
+                    if guess > secret_num:
+                        print("Hint: Guess lower.")
+                    else:
+                        print("Hint: Guess higher.")
+    else:
+        print("You lose. My number was {}.".format(secret_num))
+    play_again = input("Do you want to play again? Y/n ")
+    if play_again.lower() != 'n':
+        game()
+    else:
+        print("Good bye. Thank you for playing.")
 
-main()
+game()
