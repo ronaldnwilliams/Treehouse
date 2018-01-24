ec8c4d5ad0839ad05a37d12e85436c5f63f5c7af
Ronald Williams <ronaldnwilliams@gmail.com>
2018-01-24 14:25:34 -0600

    JavaScript - AJAX_Basics: Added a room list that lets you know if a conference room is empty or full.

--- JavaScript/AJAX_Basics/Employee_Status/js/widget.js
+++ JavaScript/AJAX_Basics/Employee_Status/js/widget.js
@@ -18,3 +18,24 @@ xhr.onreadystatechange = function () {
 };
 xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+var xhrRoom = new XMLHttpRequest();
+xhrRoom.onreadystatechange = function () {
+    if(xhrRoom.readyState === 4) {
+        var rooms = JSON.parse(xhrRoom.responseText);
+        var availableHTML = '<ul class="rooms">';
+        for (var i = 0; i<rooms.length; i += 1) {
+            if (rooms[i].available === true) {
+                availableHTML += '<li class="empty">';
+            } else {
+                availableHTML += '<li class="full">';
+            }
+            availableHTML += rooms[i].room;
+            availableHTML += '</li>';
+        }
+        availableHTML += '</ul>';
+        document.getElementById('roomList').innerHTML = availableHTML;
+    }
+};
+xhrRoom.open('GET', '../data/rooms.json');
+xhrRoom.send();
