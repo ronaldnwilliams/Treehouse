ecb1957146b60bdb0e70d64e053fd223650b018c
Ronald Williams <ronaldnwilliams@gmail.com>
2018-01-24 17:50:41 -0600

    JavaScript jQuery: Added a simple jQuery app.

---
+++ JavaScript/jQuery/hello_jQuery/js/app.js
@@ -0,0 +1,17 @@
+//const box = document.querySelector('.box');
+//box.style.display = 'none';
+
+//jQuery('.box').hide();
+/*
+$('.box').hide();
+$('.box').show();
+*/
+
+/*box.addEventListener('click', function() {
+    alert('You clicked a box!');
+});*/
+
+$('.box').click(function(){
+    alert('You clicked a box!');
+});
+
