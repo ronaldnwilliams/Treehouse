8f9518551419ab2dd7401ce4c98938a3274eb58e
Ronald Williams <ronaldnwilliams@gmail.com>
2018-02-25 10:08:31 -0600

    JavaScript - OOJS: Updated Dice app with a constructor and prototype method.

--- JavaScript/Object-Oriented-JS/dice/dice.js
+++ JavaScript/Object-Oriented-JS/dice/dice.js
@@ -1,6 +1,9 @@
-var dice = {
-  sides: 6,
-  roll: function () {
-    return Math.floor(Math.random() * this.sides) + 1;
-  }
+function Dice(sides) {
+  this.sides = sides;
 }
+
+Dice.prototype.roll = function () {
+  return Math.floor(Math.random() * this.sides) + 1;
+}
+
+var dice = new Dice(6);
