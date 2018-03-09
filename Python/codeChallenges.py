fe0856cafbba0f0b1e568c3efb886f21b02baaaa
Ronald Williams <ronaldnwilliams@gmail.com>
2018-03-09 11:59:30 -0600

    Updated.

--- Python/codeChallenges.py
+++ Python/codeChallenges.py
@@ -33,3 +33,37 @@ def covers_all(topics):
     return covered
 
 print(covers_all({"conditions", "input"}))
+
+
+
+class Letter:
+
+    def __init__(self, pattern=None):
+        self.pattern = pattern
+
+    def __iter__(self):
+        yield from self.pattern
+    def __str__(self):
+        output = []
+        for blip in self:
+            if blip == '.':
+                output.append('dot')
+            else:
+                output.append('dash')
+        return '-'.join(output)
+
+    @classmethod
+    def from_string(cls, string_pattern):
+        split_string = string_pattern.split('-')
+        pattern = []
+        for i in split_string:
+            if i == 'dash':
+                pattern.append('-')
+            if i == 'dot':
+                pattern.append('.')
+        return cls(pattern)
+
+class S(Letter):
+    def __init__(self):
+        pattern = ['.', '.', '.']
+        super().__init__(pattern)
