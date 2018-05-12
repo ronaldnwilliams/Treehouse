c81b7d0ae2604f79c091e66ef2f3972ae171db48
Ronald Williams <ronaldnwilliams@gmail.com>
2018-05-12 11:21:10 -0500

    updated to deploy on heroku and fixed templates

--- Python/Django/the_ronald_tribune/the_ronald_tribune/settings.py
+++ Python/Django/the_ronald_tribune/the_ronald_tribune/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vnl8igb@%g1a&aio^!7c8@nex5)&&psq@n*+8e5@=%um&k4zsn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-ronald-tribune.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
