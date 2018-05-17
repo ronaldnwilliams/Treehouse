00cbd13881274665c509f5d316c8aa0f6a880083
Ronald Williams <ronaldnwilliams@gmail.com>
2018-05-17 18:02:07 -0500

    Created courses nav template and tags.

--- Python/Django/basics/learning_site/courses/templatetags/course_extras.py
+++ Python/Django/basics/learning_site/courses/templatetags/course_extras.py
@@ -9,7 +9,15 @@ def newest_course():
     '''Gets the most recent course that was added to the library.'''
     return Course.objects.latest('created_at')
 
+@register.inclusion_tag('courses/course_nav.html')
 def nav_courses_list():
     ''' Returns dictionary of courses to display as navigation theme'''
     courses = Course.objects.all()
     return {'courses': courses}
+
+@register.filter('time_estimate')
+def time_estimate(word_count):
+    ''' Estimates the number of minutes it will take to complete a step
+    based on passed in word count'''
+    minutes = round(word_count/20)
+    return minutes
