98c3ac6b59ee8281c0cadc85462e2662efd9b999
Ronald Williams <ronaldnwilliams@gmail.com>
2018-05-17 19:20:52 -0500

    Added Markdown2 to requirements.txt. Added custom templatetag with markdown2 that makes text into markdown and made safe with django.utils.safestring.

--- Python/Django/basics/learning_site/courses/templatetags/course_extras.py
+++ Python/Django/basics/learning_site/courses/templatetags/course_extras.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.safestring import mark_safe
+
+import markdown2
 
 from courses.models import Course
 
@@ -21,3 +24,9 @@ def time_estimate(word_count):
     based on passed in word count'''
     minutes = round(word_count/20)
     return minutes
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+    '''Converts markdown text to html'''
+    html_body = markdown2.markdown(markdown_text)
+    return mark_safe(html_body)
